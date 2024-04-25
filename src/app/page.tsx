e2cc77c44ded7f0ed60965c28158cb4c28a382a1
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-04-25 17:15:33 +0100

    main page

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import PigGame from "./Components/Organisms/PigGame";
+
+export default function Home() {
+  return (
+    <main className="flex h-screen flex-row items-center justify-center bg-lightGray">
+      <PigGame />
+    </main>
+  );
+};
